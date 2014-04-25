979a6fdcd74c809c6c01b901c33ca8ed9035f3aa
lainredsonic <lp007819@hotmail.com>
2014-04-25 18:11:24 +0800

    [update] add exception handle and 301 process

--- dns_http_icmp_spider2.pl
+++ dns_http_icmp_spider2.pl
@@ -27,37 +27,52 @@ eval {
 };
 
 if ($@){
-	print "malform url\n";
+	die "malform url\n";
 }
 
 $path = "/" if($path eq "");
 
 
 my ($host_ip, $dns_latency, $dns_success) = &p_dns($host);
-print "$host_ip $dns_latency\n" if $dns_success;
+if($dns_success){
+	print "$host_ip $dns_latency\n"
+}else{
+	die "dns failed\n";
+}
 
 my $ua = $UA_CHROME;
 my $content;
 
 my ($http_latency, $http_success) = &p_http($host_ip, $host, $path, $ua, \$content);
-
-print $content," ",$http_latency,"\n";
+if ($http_success){
+	print $content," ",$http_latency,"\n" if $http_success;
+}else{
+	die "http failed\n";
+}
 
 sub p_dns {
 	my $host = $_[0];
 	my $host_ip;
 	my $success = 0;
-	my $dns_client = Net::DNS::Resolver->new(
-			nameserver => [qw(8.8.8.8 8.8.4.4)],
-			recurse => 1,
-			retry => 3,
-			dnsrch => 0,
-			udp_timeout => 3,
-			debug	=> 0
-			);
+	my $dns_client;
+	my $start_time;
+	my $dns_query;
+	eval {
+		$dns_client = Net::DNS::Resolver->new(
+				nameserver => [qw(8.8.8.8 8.8.4.4)],
+				recurse => 1,
+				retry => 3,
+				dnsrch => 0,
+				udp_timeout => 3,
+				debug	=> 0
+				);
+		$start_time = gettimeofday;
+		$dns_query = $dns_client->query($host, "A");
+	};
+	if ($@){
+		warn "dns request failed\n"; 
+	}
 
-	my $start_time = gettimeofday;
-	my $dns_query = $dns_client->query($host, "A");
 	my $latency = gettimeofday - $start_time;
 	if ($dns_query){
 		foreach ($dns_query->answer){
@@ -75,25 +90,41 @@ sub p_http{
 	my ($peer, $host, $path, $ua, $content) = @_;
 	my $success = 0;
 	my $start_time = gettimeofday;
-	my $browser = Net::HTTP->new(
-			'PeerAddr' => $peer,
-			'Timeout' => 10,
-			'KeepAlive' => 1,
-			);
+	my ($browser, $code, $mess, %h);
+	eval {
+		$browser = Net::HTTP->new(
+				'PeerAddr' => $peer,
+				'Timeout' => 10,
+				'KeepAlive' => 0,
+				);
 
-	$browser->write_request(
-			'GET' => $path,
-			'User-Agent' => $ua,
-			'Host' => $host,
-			);
-	while (1){
-		my $buf;
-		my $n = $browser->read_entity_body($buf, 1024);
-		die "read failed: $!" unless defined $n;
-		last unless $n;
-		$$content .= $buf;
+		$browser->write_request(
+				'GET' => $path,
+				'User-Agent' => $ua,
+				'Host' => $host,
+				);
+		($code, $mess, %h) = $browser->read_response_headers;
+		while (1){
+			my $buf;
+			my $n = $browser->read_entity_body($buf, 1024);
+			last unless $n;
+			$$content .= $buf;
+		}
+	};
+	if ($@){
+		warn "http request failed\n"; 
+	}else{
+		$success=1;
+		if ($code == 301 and $mess eq "Moved Permanently"){
+			my $location = $h{'Location'};
+			if($location =~ /^https{0,1}:\/\//){
+				$location = $';
+				chop($location);
+			}
+			print "$location\n";
+			&p_http($peer, $location, $path, $ua, $content);
+		}
 	}
 	my $latency = gettimeofday - $start_time;
-	$success=1;
 	($latency, $success);
 }
